a7b8624deb69bc4be472e188b79337342c2ed560
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2012-12-12 18:18:07 +0100

    Copying the test resources next to built classes and js file. Modifying the test to properly use the new bck2brwsr calling scheme.

--- benchmarks/matrix-multiplication/src/main/resources/org/apidesign/benchmark/matrixmul/multiplication-test.js
+++ benchmarks/matrix-multiplication/src/main/resources/org/apidesign/benchmark/matrixmul/multiplication-test.js
@@ -1,6 +1,8 @@
 MatrixTest = TestCase("MatrixTest");
 
+
 MatrixTest.prototype.testMultiplication = function() {
-  var x = new org_apidesign_benchmark_matrixmul_Main();
+  var vm = new bck2brwsr();
+  var x = vm.loadClass("org.apidesign.benchmark.matrixmul.Main");
   x.main__V_3Ljava_lang_String_2(null);
 };
