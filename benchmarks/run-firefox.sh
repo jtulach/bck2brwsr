185bc96e35e58f12359ad98871a8b9416bb3a96a
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2012-12-13 08:40:39 +0100

    Fixing licenses for benchmarks module, so it can be merged to default branch

--- benchmarks/run-firefox.sh
+++ benchmarks/run-firefox.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
+#
+# Back 2 Browser Bytecode Translator
+# Copyright (C) 2012 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. Look for COPYING file in the top folder.
+# If not, see http://opensource.org/licenses/GPL-2.0.
+#
+
 /usr/bin/firefox --display=`echo $DISPLAY` "$@"
