5c05608cfbc6c2c1290ffa2ac75eeb10396cff7e
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2013-01-03 09:14:57 +0100

    clone works on arrays

--- emul/src/main/resources/org/apidesign/vm4brwsr/emul/java_lang_String.js
+++ emul/src/main/resources/org/apidesign/vm4brwsr/emul/java_lang_String.js
@@ -6,4 +6,11 @@ Array.prototype.fillNulls = function() {
   for(var i = 0; i < this.length; i++) this[i] = null;
   return this;
 };
-
+Array.prototype.clone__Ljava_lang_Object_2 = function() {
+  var s = this.length;
+  var ret = new Array(s);
+  for (var i = 0; i < s; i++) {
+      ret[i] = this[i];
+  }
+  return ret;
+};
