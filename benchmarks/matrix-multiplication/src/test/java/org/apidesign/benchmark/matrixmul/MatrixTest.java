0a2b9077005566287b8903e81652a59d555e93fe
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2013-01-22 13:56:27 +0100

    Fixing the matrix benchmark to not throw away the result and really do ten thousand iterations

--- benchmarks/matrix-multiplication/src/test/java/org/apidesign/benchmark/matrixmul/MatrixTest.java
+++ benchmarks/matrix-multiplication/src/test/java/org/apidesign/benchmark/matrixmul/MatrixTest.java
@@ -27,8 +27,6 @@ import org.testng.annotations.Factory;
  * @author Jaroslav Tulach <jtulach@netbeans.org>
  */
 public class MatrixTest {
-    public static final int ITERATION_COUNT = 10;
-    
     public MatrixTest() {
     }
 
@@ -41,12 +39,9 @@ public class MatrixTest {
         m2.generateData();
         
         Matrix res = null;
-        for (int i = 0; i < ITERATION_COUNT; i++) {
-            Matrix m = m1.multiply(m2);
-            if (res != null && !res.equals(m)) {
-                return "different";
-            }
-            res = m;
+        for (int i = 0; i < 10000; i++) {
+            res = m1.multiply(m2);
+            m1 = res;
         }
         
         StringBuilder sb = new StringBuilder();
