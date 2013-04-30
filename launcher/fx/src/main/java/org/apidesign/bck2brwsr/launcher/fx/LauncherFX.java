e1fce56a8bba8d26f08c4678acb3350d408ed909
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2013-04-30 16:32:16 +0200

    One more fake Javadoc class

---
+++ launcher/fx/src/main/java/org/apidesign/bck2brwsr/launcher/fx/LauncherFX.java
@@ -0,0 +1,32 @@
+/**
+ * Back 2 Browser Bytecode Translator
+ * Copyright (C) 2012 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://opensource.org/licenses/GPL-2.0.
+ */
+package org.apidesign.bck2brwsr.launcher.fx;
+
+import org.apidesign.bck2brwsr.launcher.Launcher;
+
+/** This is a launcher for the <a href="http://bck2brwsr.apidesign.org">Bck2Brwsr</a>
+ * project that is using <b>JavaFX</b>'s WebView to display the browser inside
+ * real Java virtual machine. Use {@link Launcher} methods to access this
+ * functionality via public, supported methods.
+ *
+ * @author Jaroslav Tulach
+ */
+public final class LauncherFX {
+    private LauncherFX() {
+    }
+}
