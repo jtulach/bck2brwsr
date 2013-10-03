bc219ae41acc9bd5b9a3360af3b33e34e841f439
Jaroslav Tulach <jtulach@netbeans.org>
2013-10-03 10:02:13 +0200

    Fixing license

--- dew/src/main/java/org/apidesign/bck2brwsr/dew/BaseFileObject.java
+++ dew/src/main/java/org/apidesign/bck2brwsr/dew/BaseFileObject.java
@@ -1,7 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Back 2 Browser Bytecode Translator
+ * Copyright (C) 2012 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://opensource.org/licenses/GPL-2.0.
  */
 
 package org.apidesign.bck2brwsr.dew;
@@ -12,7 +24,7 @@ import javax.lang.model.element.NestingKind;
 
 /**
  *
- * @author tom
+ * @author Tomas Zezula
  */
 public abstract class BaseFileObject implements InferableJavaFileObject {
 
--- dew/src/main/java/org/apidesign/bck2brwsr/dew/ClassLoaderFileManager.java
+++ dew/src/main/java/org/apidesign/bck2brwsr/dew/ClassLoaderFileManager.java
@@ -1,16 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Back 2 Browser Bytecode Translator
+ * Copyright (C) 2012 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://opensource.org/licenses/GPL-2.0.
  */
 
 package org.apidesign.bck2brwsr.dew;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
