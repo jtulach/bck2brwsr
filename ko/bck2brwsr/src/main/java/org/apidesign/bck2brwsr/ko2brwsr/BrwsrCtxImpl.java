eb7d4190a607619008e4828e817805cdc68b44c6
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2014-01-09 16:38:15 +0100

    Removing useless methods and classes

--- ko/bck2brwsr/src/main/java/org/apidesign/bck2brwsr/ko2brwsr/BrwsrCtxImpl.java
+++ ko/bck2brwsr/src/main/java/org/apidesign/bck2brwsr/ko2brwsr/BrwsrCtxImpl.java
@@ -21,6 +21,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import org.apidesign.bck2brwsr.core.JavaScriptBody;
 import org.apidesign.html.json.spi.JSONCall;
 import org.apidesign.html.json.spi.Transfer;
 import org.apidesign.html.json.spi.WSTransfer;
@@ -36,7 +37,7 @@ final class BrwsrCtxImpl implements Transfer, WSTransfer<LoadWS> {
     
     @Override
     public void extract(Object obj, String[] props, Object[] values) {
-        ConvertTypes.extractJSON(obj, props, values);
+        extractJSON(obj, props, values);
     }
 
     @Override
@@ -71,8 +72,8 @@ final class BrwsrCtxImpl implements Transfer, WSTransfer<LoadWS> {
         R success = new R(true);
         R failure = new R(false);
         if (call.isJSONP()) {
-            String me = ConvertTypes.createJSONP(success.arr, success);
-            ConvertTypes.loadJSONP(call.composeURL(me), me);
+            String me = createJSONP(success.arr, success);
+            loadJSONP(call.composeURL(me), me);
         } else {
             String data = null;
             if (call.isDoOutput()) {
@@ -84,7 +85,7 @@ final class BrwsrCtxImpl implements Transfer, WSTransfer<LoadWS> {
                     call.notifyError(ex);
                 }
             }
-            ConvertTypes.loadJSON(call.composeURL(null), success.arr, success, failure, call.getMethod(), data);
+            loadJSON(call.composeURL(null), success.arr, success, failure, call.getMethod(), data);
         }
     }
 
@@ -99,7 +100,7 @@ final class BrwsrCtxImpl implements Transfer, WSTransfer<LoadWS> {
             }
             sb.append((char)ch);
         }
-        return ConvertTypes.parse(sb.toString());
+        return parse(sb.toString());
     }
 
     @Override
@@ -116,4 +117,94 @@ final class BrwsrCtxImpl implements Transfer, WSTransfer<LoadWS> {
     public void close(LoadWS socket) {
         socket.close();
     }
+    
+    //
+    // implementations
+    //
+    
+    @JavaScriptBody(args = {"object", "property"},
+        body
+        = "if (property === null) return object;\n"
+        + "if (object === null) return null;\n"
+        + "var p = object[property]; return p ? p : null;"
+    )
+    private static Object getProperty(Object object, String property) {
+        return null;
+    }
+
+    public static String createJSONP(Object[] jsonResult, Runnable whenDone) {
+        int h = whenDone.hashCode();
+        String name;
+        for (;;) {
+            name = "jsonp" + Integer.toHexString(h);
+            if (defineIfUnused(name, jsonResult, whenDone)) {
+                return name;
+            }
+            h++;
+        }
+    }
+
+    @JavaScriptBody(args = {"name", "arr", "run"}, body
+        = "if (window[name]) return false;\n "
+        + "window[name] = function(data) {\n "
+        + "  delete window[name];\n"
+        + "  var el = window.document.getElementById(name);\n"
+        + "  el.parentNode.removeChild(el);\n"
+        + "  arr[0] = data;\n"
+        + "  run.run__V();\n"
+        + "};\n"
+        + "return true;\n"
+    )
+    private static boolean defineIfUnused(String name, Object[] arr, Runnable run) {
+        return true;
+    }
+
+    @JavaScriptBody(args = {"s"}, body = "return eval('(' + s + ')');")
+    static Object parse(String s) {
+        return s;
+    }
+
+    @JavaScriptBody(args = {"url", "arr", "callback", "onError", "method", "data"}, body = ""
+        + "var request = new XMLHttpRequest();\n"
+        + "if (!method) method = 'GET';\n"
+        + "request.open(method, url, true);\n"
+        + "request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');\n"
+        + "request.onreadystatechange = function() {\n"
+        + "  if (this.readyState!==4) return;\n"
+        + "  try {\n"
+        + "    arr[0] = eval('(' + this.response + ')');\n"
+        + "  } catch (error) {;\n"
+        + "    arr[0] = this.response;\n"
+        + "  }\n"
+        + "  callback.run__V();\n"
+        + "};\n"
+        + "request.onerror = function (e) {\n"
+        + "  arr[0] = e; onError.run__V();\n"
+        + "}\n"
+        + "if (data) request.send(data);"
+        + "else request.send();"
+    )
+    static void loadJSON(
+        String url, Object[] jsonResult, Runnable whenDone, Runnable whenErr, String method, String data
+    ) {
+    }
+
+    @JavaScriptBody(args = {"url", "jsonp"}, body
+        = "var scrpt = window.document.createElement('script');\n "
+        + "scrpt.setAttribute('src', url);\n "
+        + "scrpt.setAttribute('id', jsonp);\n "
+        + "scrpt.setAttribute('type', 'text/javascript');\n "
+        + "var body = document.getElementsByTagName('body')[0];\n "
+        + "body.appendChild(scrpt);\n"
+    )
+    static void loadJSONP(String url, String jsonp) {
+
+    }
+
+    public static void extractJSON(Object jsonObject, String[] props, Object[] values) {
+        for (int i = 0; i < props.length; i++) {
+            values[i] = getProperty(jsonObject, props[i]);
+        }
+    }
+    
 }
