e9bdf96b0e96bc45e2d2c86cdf4d46ee32f62b1c
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2016-01-30 18:33:49 +0100

    Adding JBox2D sample into our benchmarks

---
+++ benchmarks/jbox2d/src/test/java/org/apidesign/benchmark/jbox2d/JBox2dSimulationTest.java
@@ -0,0 +1,42 @@
+/**
+ * Back 2 Browser Bytecode Translator
+ * Copyright (C) 2012-2015 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://opensource.org/licenses/GPL-2.0.
+ */
+package org.apidesign.benchmark.jbox2d;
+
+import java.io.IOException;
+import org.apidesign.bck2brwsr.vmtest.Compare;
+import org.apidesign.bck2brwsr.vmtest.VMTest;
+import org.testng.annotations.Factory;
+
+public class JBox2dSimulationTest {
+    public JBox2dSimulationTest() {
+    }
+
+    @Compare(slowdown = 40.0)
+    public int tenThousand() throws IOException {
+        Scene s = new Scene();
+        for (int i = 0; i < 10000; i++) {
+            s.calculate();
+        }
+        return s.getWorld().getBodyCount();
+    }
+
+    @Factory
+    public static Object[] create() {
+        return VMTest.create(JBox2dSimulationTest.class);
+    }
+}
