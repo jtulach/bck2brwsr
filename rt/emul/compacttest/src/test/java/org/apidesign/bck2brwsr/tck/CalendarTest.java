1813992b1840dab94c34d7885be2669ff90bae4d
eppleton <info@eppleton.de>
2017-01-21 16:30:17 +0100

    Test for Calendar

---
+++ rt/emul/compacttest/src/test/java/org/apidesign/bck2brwsr/tck/CalendarTest.java
@@ -0,0 +1,52 @@
+/**
+ * Back 2 Browser Bytecode Translator
+ * Copyright (C) 2012-2015 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. Look for COPYING file in the top folder. If not, see
+ * http://opensource.org/licenses/GPL-2.0.
+ */
+package org.apidesign.bck2brwsr.tck;
+
+import java.util.Calendar;
+import org.apidesign.bck2brwsr.vmtest.Compare;
+import org.apidesign.bck2brwsr.vmtest.VMTest;
+import org.testng.annotations.Factory;
+
+/**
+ *
+ * @author Anton Epple <toni.epple@eppleton.de>
+ */
+public class CalendarTest {
+    
+    public @Compare
+    Calendar getInstanceTest() {
+        return Calendar.getInstance();
+    }
+    
+    public @Compare long getTimeinMillisTest() {
+        Calendar instance = Calendar.getInstance();
+        instance.setTimeInMillis(1234567);
+        return instance.getTimeInMillis();
+    }
+    
+    public @Compare long addTest() {
+        Calendar instance = Calendar.getInstance();
+        instance.setTimeInMillis(1234567);
+        instance.add(Calendar.DAY_OF_MONTH, -1);
+        return instance.getTimeInMillis();
+    }
+    
+    @Factory public static Object[] create() {
+        return VMTest.create(CalendarTest.class);
+    }
+}
