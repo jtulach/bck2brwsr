c52734961a2c5cdb6fec6045d3b6f9a93386f102
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2017-05-21 07:43:36 +0200

    Testing some kotlin code

---
+++ rt/emul/kotlintest/src/test/java/org/apidesign/bck2brwsr/kotlintest/SplitTest.kt
@@ -0,0 +1,38 @@
+/**
+ * Back 2 Browser Bytecode Translator
+ * Copyright (C) 2012-2017 Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://opensource.org/licenses/GPL-2.0.
+ */
+package org.apidesign.bck2brwsr.kotlintest
+
+import org.apidesign.bck2brwsr.vmtest.Compare;
+import org.apidesign.bck2brwsr.vmtest.VMTest;
+import org.testng.annotations.Factory;
+
+class SplitTest {
+    @Compare
+    fun splitWordsIntoParts() : String {
+        val t = "Hello World!"
+        val words = t.split(" ")
+        return words[1] + "_" + words[0];
+    }
+
+    companion object {
+        @Factory @JvmStatic
+        fun create(): Array<out Any> {
+            return VMTest.create(SplitTest::class.java);
+        }
+    }
+}
