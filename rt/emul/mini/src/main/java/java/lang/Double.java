6fc509d04d147774d52b4c20c848de546d36aba9
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2018-03-07 05:55:45 +0100

    Reimplementation using __bit64 operations

--- rt/emul/mini/src/main/java/java/lang/Double.java
+++ rt/emul/mini/src/main/java/java/lang/Double.java
@@ -939,23 +939,27 @@ public final class Double extends Number implements Comparable<Double> {
      */
     @JavaScriptBody(args={ "bits" },
         body=
-          "var hi = bits.high32();\n"
+          "var high32 = Number.prototype['__bit64']['high32'];\n"
+        + "var low32 = Number.prototype['__bit64']['low32'];\n"
+        + "var toFP = Number.prototype['__bit64']['toFP'];\n"
+        + "var hi = high32(bits);\n"
+        + "var lb = low32(bits);\n"
         + "var s = (hi & 0x80000000) === 0 ? 1 : -1;\n"
         + "var e = (hi >> 20) & 0x7ff;\n"
         + "if (e === 0x7ff) {\n"
-        + "  if ((bits == 0) && ((hi & 0xfffff) === 0)) {\n"
+        + "  if ((lb == 0) && ((hi & 0xfffff) === 0)) {\n"
         + "    return (s > 0) ? Number.POSITIVE_INFINITY"
                           + " : Number.NEGATIVE_INFINITY;\n"
         + "  }\n"
         + "  return Number.NaN;\n"
         + "}\n"
-        + "var m = (hi & 0xfffff).next32(bits);\n"
+        + "var m = (hi & 0xfffff).next32(lb);\n"
         + "if (e === 0) {\n"
         + "  m = __shl64(m, 1);\n"
         + "} else {\n"
-        + "  m.hi = m.high32() | 0x100000;\n"
+        + "  m = (high32(m) | 0x100000).next32(lb);\n"
         + "}\n"
-        + "return s * m.toFP() * Math.pow(2.0, e - 1075);\n"
+        + "return s * toFP(m) * Math.pow(2.0, e - 1075);\n"
     )
     public static native double longBitsToDouble(long bits);
 
