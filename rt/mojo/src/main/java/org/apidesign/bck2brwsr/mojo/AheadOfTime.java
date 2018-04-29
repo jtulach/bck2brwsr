1ba6b58161eec8413597d65961ab6f80e7297e67
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2018-04-29 07:26:55 +0200

    Using singletonizer pattern to isolate the logical of AheadOfTime task from dependency on Maven

--- rt/mojo/src/main/java/org/apidesign/bck2brwsr/mojo/AheadOfTime.java
+++ rt/mojo/src/main/java/org/apidesign/bck2brwsr/mojo/AheadOfTime.java
@@ -18,24 +18,7 @@
 package org.apidesign.bck2brwsr.mojo;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.jar.Attributes;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
-import java.util.zip.ZipEntry;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -45,8 +28,6 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
-import org.apidesign.bck2brwsr.aot.Bck2BrwsrJars;
-import org.apidesign.vm4brwsr.Bck2Brwsr;
 import org.apidesign.vm4brwsr.ObfuscationLevel;
 
 /**
@@ -98,133 +79,96 @@ public class AheadOfTime extends AbstractMojo {
     
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        URLClassLoader loader;
-        try {
-            loader = buildClassLoader(mainJar, prj.getArtifacts());
-        } catch (MalformedURLException ex) {
-            throw new MojoFailureException("Can't initialize classloader");
-        }
-        for (Artifact a : prj.getArtifacts()) {
-            if (a.getFile() == null) {
-                continue;
+        class Work extends AheadOfTimeBase<Artifact> {
+            @Override
+            protected File mainJavaScript() {
+                return mainJavaScript;
             }
-            String n = a.getFile().getName();
-            if (!n.endsWith(".jar")) {
-                continue;
-            }
-            if ("provided".equals(a.getScope())) {
-                continue;
-            }
-            File aot = new File(mainJavaScript.getParent(), classPathPrefix);
-            aot.mkdirs();
-            File js = new File(aot, n.substring(0, n.length() - 4) + ".js");
-            if (js.lastModified() > a.getFile().lastModified()) {
-                getLog().info("Skipping " + js + " as it already exists.");
-                continue;
-            }
-            try {
-                aotLibrary(a, js , loader);
-            } catch (IOException ex) {
-                throw new MojoFailureException("Can't compile " + a.getFile(), ex);
-            }
-        }
-        
-        try {
-            if (mainJavaScript.lastModified() > mainJar.lastModified()) {
-                getLog().info("Skipping " + mainJavaScript + " as it already exists.");
-            } else {
-                getLog().info("Generating " + mainJavaScript);
-                Bck2Brwsr c = Bck2BrwsrJars.configureFrom(null, mainJar, loader, ignoreBootClassPath);
-                if (exports != null) {
-                    for (String e : exports) {
-                        if (e != null) {
-                            c = c.addExported(e.replace('.', '/'));
-                        }
-                    }
-                }
-                Writer w = new OutputStreamWriter(new FileOutputStream(mainJavaScript), "UTF-8");
-                c.
-                        obfuscation(obfuscation).
-                        generate(w);
-                w.close();
-            }
-        } catch (IOException ex) {
-            throw new MojoFailureException("Cannot generate script for " + mainJar, ex);
-        }
-            
-        try {
-            Writer w = new OutputStreamWriter(new FileOutputStream(vm), "UTF-8");
-            Bck2Brwsr.newCompiler().
-                    obfuscation(obfuscation).
-                    standalone(false).
-                    resources(new Bck2Brwsr.Resources() {
 
-                @Override
-                public InputStream get(String resource) throws IOException {
-                    return null;
-                }
-            }).
-                    generate(w);
-            w.close();
-            
-        } catch (IOException ex) {
-            throw new MojoExecutionException("Can't compile", ex);
-        }
-    }
+            @Override
+            protected String classPathPrefix() {
+                return classPathPrefix;
+            }
 
-    private void aotLibrary(Artifact a, File js, URLClassLoader loader) throws IOException, MojoExecutionException {
-        for (Artifact b : prj.getArtifacts()) {
-            if ("bck2brwsr".equals(b.getClassifier())) { // NOI18N
-                getLog().debug("Inspecting " + b.getFile());
-                JarFile jf = new JarFile(b.getFile());
-                Manifest man = jf.getManifest();
-                for (Map.Entry<String, Attributes> entrySet : man.getEntries().entrySet()) {
-                    String entryName = entrySet.getKey();
-                    Attributes attr = entrySet.getValue();
-                    
-                    if (
-                        a.getArtifactId().equals(attr.getValue("Bck2BrwsrArtifactId")) &&
-                        a.getGroupId().equals(attr.getValue("Bck2BrwsrGroupId")) &&
-                        a.getVersion().equals(attr.getValue("Bck2BrwsrVersion")) &&
-                        (
-                            obfuscation == ObfuscationLevel.FULL && "true".equals(attr.getValue("Bck2BrwsrMinified"))
-                            ||
-                            obfuscation != ObfuscationLevel.FULL && "true".equals(attr.getValue("Bck2BrwsrDebug"))
-                        )
-                    ) {
-                        getLog().info("Extracting " + js + " from " + b.getFile());
-                        InputStream is = jf.getInputStream(new ZipEntry(entryName));
-                        Files.copy(is, js.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                        is.close();
-                        return;
-                    }
+            @Override
+            protected ObfuscationLevel obfuscation() {
+                return obfuscation;
+            }
+
+            @Override
+            protected String[] exports() {
+                return exports;
+            }
+
+            @Override
+            protected boolean ignoreBootClassPath() {
+                return ignoreBootClassPath;
+            }
+
+            @Override
+            protected boolean generateAotLibraries() {
+                return generateAotLibraries;
+            }
+
+            @Override
+            protected File mainJar() {
+                return mainJar;
+            }
+
+            @Override
+            protected File vm() {
+                return vm;
+            }
+
+            @Override
+            protected Collection<Artifact> artifacts() {
+                return prj.getArtifacts();
+            }
+
+            @Override
+            protected void logInfo(String msg) {
+                getLog().info(msg);
+            }
+
+            @Override
+            protected Exception failure(String msg, Throwable cause) {
+                if (cause != null) {
+                    return new MojoFailureException(msg, cause);
+                } else {
+                    return new MojoExecutionException(msg);
                 }
             }
-        }
-        if (!generateAotLibraries) {
-            throw new MojoExecutionException("Not generating " + js + " and no precompiled version found!");
-        }
-        getLog().info("Generating " + js);
-        Writer w = new OutputStreamWriter(new FileOutputStream(js), "UTF-8");
-        Bck2Brwsr c = Bck2BrwsrJars.configureFrom(null, a.getFile(), loader, ignoreBootClassPath);
-        if (exports != null) {
-            c = c.addExported(exports);
-        }
-        c.
-            obfuscation(obfuscation).
-            generate(w);
-        w.close();
-    }
-    private static URLClassLoader buildClassLoader(File root, Collection<Artifact> deps) throws MalformedURLException {
-        List<URL> arr = new ArrayList<URL>();
-        if (root != null) {
-            arr.add(root.toURI().toURL());
-        }
-        for (Artifact a : deps) {
-            if (a.getFile() != null) {
-                arr.add(a.getFile().toURI().toURL());
+
+            @Override
+            protected File file(Artifact a) {
+                return a.getFile();
+            }
+
+            @Override
+            protected String scope(Artifact a) {
+                return a.getScope();
+            }
+
+            @Override
+            protected String classifier(Artifact a) {
+                return a.getClassifier();
+            }
+
+            @Override
+            protected String artifactId(Artifact a) {
+                return a.getArtifactId();
+            }
+
+            @Override
+            protected String groupId(Artifact a) {
+                return a.getGroupId();
+            }
+
+            @Override
+            protected String version(Artifact a) {
+                return a.getVersion();
             }
         }
-        return new URLClassLoader(arr.toArray(new URL[0]), Java2JavaScript.class.getClassLoader());
+        new Work().work();
     }
 }
