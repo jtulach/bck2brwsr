02f593e7b1d2ac5dcd963c4c9df6607d3aa91a4d
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2021-07-08 08:39:42 +0200

    ArrayList has to implement List due to check in PropertyBinding

--- ko/bck2brwsr/src/test/java/java/util/ArrayList.java
+++ ko/bck2brwsr/src/test/java/java/util/ArrayList.java
@@ -20,7 +20,7 @@ package java.util;
 import net.java.html.json.Models;
 import org.apidesign.bck2brwsr.ko2brwsr.Bck2BrwsrJavaScriptImpl;
 
-public class ArrayList<T> {
+public class ArrayList<T> implements List<T> {
     static {
         Bck2BrwsrJavaScriptImpl.init();
     }
@@ -40,5 +40,110 @@ public class ArrayList<T> {
 
     public int size() {
         return list.size();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return list.toArray(a);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return list.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return list.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return list.addAll(c);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return list.addAll(index, c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return list.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return list.retainAll(c);
+    }
+
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return list.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return list.hashCode();
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return list.set(index, element);
+    }
+
+    @Override
+    public void add(int index, T element) {
+        list.add(index, element);
+    }
+
+    @Override
+    public T remove(int index) {
+        return list.remove(index);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return list.listIterator();
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return list.listIterator(index);
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return list.subList(fromIndex, toIndex);
     }
 }
