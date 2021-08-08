61d1787ff7a42b9c69e372691b3796ce9769bdae
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2021-08-08 09:55:02 +0200

    Done checkbox and clear button

--- src/main/java/org/frontend/app/Demo.java
+++ src/main/java/org/frontend/app/Demo.java
@@ -3,7 +3,9 @@ package org.frontend.app;
 import com.dukescript.api.javafx.beans.FXBeanInfo;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.IntegerBinding;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -14,17 +16,36 @@ import static net.java.html.json.Models.applyBindings;
 public final class Demo extends DemoBeanInfo {
 
     final StringProperty desc = new SimpleStringProperty(this, "desc", "");
-    final ListProperty<String> todos = new SimpleListProperty<>(this, "todos", FXCollections.observableArrayList());
+    final ListProperty<Item> todos = new SimpleListProperty<>(this, "todos", FXCollections.observableArrayList());
     final IntegerBinding numTodos = Bindings.createIntegerBinding(todos::size, todos);
-    
+
     void addTodo() {
-        todos.getValue().add(desc.getValue());
+        todos.getValue().add(new Item(desc.getValue()));
         desc.setValue("");
     }
-    
+
+    void clearTodos() {
+        todos.getValue().removeIf((item) -> item.done.get());
+    }
+
+    @FXBeanInfo.Generate
+    final class Item extends ItemBeanInfo {
+        final StringProperty desc = new SimpleStringProperty();
+        final BooleanProperty done = new SimpleBooleanProperty();
+
+        public Item(String desc) {
+            this.desc.setValue(desc);
+        }
+
+        void check() {
+            this.done.set(!this.done.get());
+        }
+    }
+
     public static void onPageLoad() {
         Demo model = new Demo();
         model.desc.setValue("Try Java in browser @ " + System.currentTimeMillis());
         applyBindings(model);
     }
+
 }
