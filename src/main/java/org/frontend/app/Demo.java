21a93edf194c1efeff8f71f6acef4bea41b3954d
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2021-10-10 15:35:45 +0200

    Tutorial with preview card

--- src/main/java/org/frontend/app/Demo.java
+++ src/main/java/org/frontend/app/Demo.java
@@ -3,12 +3,15 @@ package org.frontend.app;
 import com.dukescript.api.javafx.beans.FXBeanInfo;
 import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.IntegerBinding;
 import javafx.beans.binding.NumberBinding;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
@@ -16,8 +19,8 @@ import static net.java.html.json.Models.applyBindings;
 
 @FXBeanInfo.Generate
 public final class Demo extends DemoBeanInfo {
-
     final StringProperty desc = new SimpleStringProperty(this, "desc", "");
+    final ObjectProperty<Item> selected = new SimpleObjectProperty<>(null);
     final ListProperty<Item> todos = new SimpleListProperty<>(this, "todos", FXCollections.observableArrayList(item -> new Observable[] { item.done }));
     final IntegerBinding numTodos = Bindings.createIntegerBinding(todos::size, todos);
     final IntegerBinding numPending = Bindings.createIntegerBinding(() -> {
@@ -26,31 +29,61 @@ public final class Demo extends DemoBeanInfo {
     final NumberBinding numFinished = Bindings.subtract(numTodos, numPending);
 
     void addTodo() {
-        todos.getValue().add(new Item(desc.getValue()));
+        todos.getValue().add(new Item(desc.getValue(), null, null));
         desc.setValue("");
     }
 
     void clearTodos() {
-        todos.getValue().removeIf((item) -> item.done.get());
+        todos.removeIf((item) -> item.done.get());
+        if (!todos.contains(selected.getValue())) {
+            selected.setValue(todos.isEmpty() ? null : todos.get(0));
+        }
+    }
+
+    void addItem(Item item) {
+        todos.add(item);
+        if (selected.getValue() == null) {
+            selected.setValue(item);
+        }
     }
 
     @FXBeanInfo.Generate
     final class Item extends ItemBeanInfo {
-        final StringProperty desc = new SimpleStringProperty();
+        final String desc;
         final BooleanProperty done = new SimpleBooleanProperty();
+        final BooleanBinding selected = Bindings.createBooleanBinding(() -> this == Demo.this.selected.getValue(), Demo.this.selected);
+        final String html;
+        final String url;
 
-        public Item(String desc) {
-            this.desc.setValue(desc);
+        public Item(String desc, String html, String url) {
+            this.desc = desc;
+            this.html = html;
+            this.url = url;
         }
 
         void check() {
             this.done.set(!this.done.get());
+        }
+
+        void select() {
+            Demo.this.selected.setValue(this);
         }
     }
 
     public static void onPageLoad() {
         Demo model = new Demo();
         model.desc.setValue("Enjoy Java in browser!");
+        model.addItem(model.new Item(
+            "Why Java in a browser?",
+            "<p>Hi there!</p>",
+            "http://apidesign.org"
+        ));
+        model.addItem(model.new Item(
+            "Yes HTML in a browser?",
+            "<p>Hello there!</p>",
+            "http://xelfi.cz"
+        ));
+        model.addItem(model.new Item("Ahoj", "Kuk", "xyz"));
         applyBindings(model);
     }
 
