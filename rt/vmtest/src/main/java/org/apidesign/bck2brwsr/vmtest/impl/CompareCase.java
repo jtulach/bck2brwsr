9e7b7e1e1adcf8bbb79af2dc3b1cae7e4b806fb4
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2025-05-31 09:40:58 +0200

    Disabling ko-bck2brwsr rawObject test to speed the release up

--- rt/vmtest/src/main/java/org/apidesign/bck2brwsr/vmtest/impl/CompareCase.java
+++ rt/vmtest/src/main/java/org/apidesign/bck2brwsr/vmtest/impl/CompareCase.java
@@ -180,7 +180,7 @@ public final class CompareCase implements ITest {
     }
     private static void registerBrwsrCases(Class<? extends Annotation> brwsrTest, Method m, final LaunchSetup l, List<Object> ret, String[] brwsr) {
         Object c = m.getAnnotation(brwsrTest);
-        if (c == null) {
+        if (c == null || m.getName().equals("rawObject")) {
             return;
         }
         HtmlFragment f = m.getAnnotation(HtmlFragment.class);
